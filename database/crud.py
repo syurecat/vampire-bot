ddae5db34ecaffa737c1b5ec011c06bad96f3ab8
syurecat <syurecat0@gmail.com>
2025-08-05 13:42:01 +0900

    update command and add ranking

--- database/crud.py
+++ database/crud.py
@@ -4,6 +4,7 @@ from . import SessionLocal
 from .models import Guild, User, GuildUser, VCSummary, VCSession
 import logging
 import time
+from typing import Sequence
 from datetime import datetime, timezone
 from contextlib import contextmanager
 
@@ -16,6 +17,8 @@ def get_session():
     try:
         yield session
     except FutureDateError:
+        raise
+    except NoDataError:
         raise
     except Exception as e:
         session.rollback()
@@ -31,15 +34,73 @@ class FormatTime:
         self.hour = hour
         self.minute = minute
         self.second = second
+
     def __str__(self):
         return f'{self.hour:03d}時間 {self.minute:02d}分 {self.second:02d}秒'
+    
+    def to_dict(self):
+        return {
+            "hour": self.hour,
+            "minute": self.minute,
+            "second": self.second
+        }
 
 
 def formatTime(seconds: int):
+    if not isinstance(seconds, int):
+        raise TypeError("The argument must be of type int.")
     hour = seconds // 3600
     minute = (seconds % 3600) // 60
     second = seconds % 60
     return FormatTime(hour, minute, second)
+
+class VCRankingEntry:
+    def __init__(self, rank: int, user_id: int, total_connection_time: FormatTime, total_mic_on_time: FormatTime):
+        self.rank = rank
+        self.user_id = user_id
+
+        if not isinstance(total_connection_time, FormatTime):
+            total_connection_time = formatTime(total_connection_time)
+        if not isinstance(total_mic_on_time, FormatTime):
+            total_mic_on_time = formatTime(total_mic_on_time)
+
+        self.total_connection_time = total_connection_time
+        self.total_mic_on_time = total_mic_on_time
+
+    def to_dict(self):
+        return {
+            "rank": self.rank,
+            "user_id": self.user_id,
+            "connection_time": self.total_connection_time.to_dict(),
+            "mic_on_time": self.total_mic_on_time.to_dict()
+        }
+    
+    def __repr__(self):
+        return f"<VCRankingEntry(user_id={self.user_id}, connection_time='{self.total_connection_time}', mic_on_time='{self.total_mic_on_time}')>"
+
+
+class VCRankingList:
+    def __init__(self, entries: Sequence[VCRankingEntry]):
+        self.entries = list(entries)
+
+    def top_n(self, n: int):
+        return self.entries[:n]
+
+    def to_dict(self):
+        return [entry.to_dict() for entry in self.entries]
+    
+    def __repr__(self):
+        return f"<VCRankingList(entries={self.entries})>"
+
+def convertRankingTuplesToList(ranking_tuples: Sequence[tuple]) -> list[VCRankingEntry]:
+    entries = []
+    for rank, (user_id, total_connection_time, total_mic_on_time) in enumerate(ranking_tuples, start=1):
+        if not isinstance(total_connection_time, FormatTime):
+            total_connection_time = formatTime(total_connection_time)
+        if not isinstance(total_mic_on_time, FormatTime):
+            total_mic_on_time = formatTime(total_mic_on_time)
+        entries.append(VCRankingEntry(rank, user_id, total_connection_time, total_mic_on_time))
+    return entries
 
 
 def checkExistsGuild(session: Session, guild_id: int):
@@ -115,11 +176,14 @@ def addUserCount(session: Session, user_id: int):
 class FutureDateError(ValueError):
     pass
 
+class NoDataError(ValueError):
+    pass
+
 def readVcSummary(session: Session, guild_id: int, user_id: int, channel_id: int, year: int = None, month: int = None):
     checkExistsGuildUser(session, guild_id, user_id)
     now_utc = datetime.now(timezone.utc)
     if (year or now_utc.year, month or 1) > (now_utc.year, now_utc.month):
-        logger.warning(f"Input error: The year and month are in the future.")
+        logger.debug(f"Input error: The year and month are in the future.")
         raise FutureDateError("指定された年月は未来です")
 
     guild_user = session.query(GuildUser).filter_by(guild_id=guild_id, user_id=user_id).one()
@@ -130,12 +194,69 @@ def readVcSummary(session: Session, guild_id: int, user_id: int, channel_id: int
     else:
         year = year or now_utc.year
         month = month or now_utc.month
-        checkExistsVCSummary(session, id=guild_user.id, channel_id=channel_id, year=year, month=month)
-        vc_summary = session.query(VCSummary).filter_by(id=guild_user.id, channel_id=channel_id, year=year, month=month).one()
+        vc_summary = session.query(VCSummary).filter_by(id=guild_user.id, channel_id=channel_id, year=year, month=month).one_or_none()
+        if vc_summary is None:
+            logger.debug(f"No VCSummary data found")
+            raise NoDataError
         connection_time = formatTime(vc_summary.total_connection_time)
         mic_on_time = formatTime(vc_summary.total_mic_on_time)
     return connection_time, mic_on_time
 
+def readVcRankEntries(session: Session, guild_id: int, channel_id: int = None, year: int = None, month: int = None, limit: int = 10):
+    checkExistsGuild(session, guild_id)
+    now_utc = datetime.now(timezone.utc)
+    if (year or now_utc.year, month or 1) > (now_utc.year, now_utc.month):
+        logger.warning(f"Input error: The year and month are in the future.")
+        raise FutureDateError("指定された年月は未来です")
+    
+    year = year or now_utc.year
+    query = session.query(GuildUser.user_id, func.sum(VCSummary.total_connection_time).label("total_connection_time"), func.sum(VCSummary.total_mic_on_time).label("total_mic_on_time")
+                          ).join(VCSummary, VCSummary.id == GuildUser.id).filter(GuildUser.guild_id == guild_id, VCSummary.year == year)
+    
+    if channel_id is not None:
+        query = query.filter(VCSummary.channel_id == channel_id)
+
+    if month is not None:
+        query = query.filter(VCSummary.month == month)
+
+    ranking_tuples = query.group_by(GuildUser.user_id).order_by((func.sum(VCSummary.total_connection_time) - func.sum(VCSummary.total_mic_on_time)).desc(), func.sum(VCSummary.total_connection_time).desc()).limit(limit).all()
+    ranking = convertRankingTuplesToList(ranking_tuples)
+    return VCRankingList(ranking)
+
+def readUserVcRankEntry(session: Session, guild_id: int, user_id: int, channel_id: int = None, year: int = None, month: int = None, limit: int = 10):
+    checkExistsGuildUser(session, guild_id, user_id)
+    now_utc = datetime.now(timezone.utc)
+    if (year or now_utc.year, month or 1) > (now_utc.year, now_utc.month):
+        logger.warning(f"Input error: The year and month are in the future.")
+        raise FutureDateError("指定された年月は未来です")
+    
+    year = year or now_utc.year
+    user_query = (session.query(GuildUser.user_id, func.sum(VCSummary.total_connection_time).label("total_connection_time"), func.sum(VCSummary.total_mic_on_time).label("total_mic_on_time"))
+                  .join(VCSummary, VCSummary.id == GuildUser.id).filter(GuildUser.guild_id == guild_id, VCSummary.year == year))
+    
+    diff_subquery = (session.query(GuildUser.user_id, (func.sum(VCSummary.total_connection_time) - func.sum(VCSummary.total_mic_on_time)).label("diff_time"))
+                            .join(VCSummary, VCSummary.id == GuildUser.id).filter(GuildUser.guild_id == guild_id, VCSummary.year == year))
+    
+    if channel_id is not None:
+        user_query = user_query.filter(VCSummary.channel_id == channel_id)
+        diff_subquery = diff_subquery.filter(VCSummary.channel_id == channel_id)
+
+    if month is not None:
+        user_query = user_query.filter(VCSummary.month == month)
+        diff_subquery = diff_subquery.filter(VCSummary.month == month)
+
+    my_totals = user_query.filter(GuildUser.user_id == user_id).one_or_none()
+    diff_subquery = diff_subquery.group_by(GuildUser.user_id).subquery()
+
+    if my_totals.total_connection_time is None or my_totals.total_mic_on_time is None:
+        logger.debug(f"No VCSummary data found")
+        raise NoDataError
+    
+    my_diff = my_totals.total_connection_time - my_totals.total_mic_on_time
+    
+    rank = (session.query(func.count()).select_from(diff_subquery).filter(diff_subquery.c.diff_time > my_diff).scalar()) + 1
+
+    return VCRankingEntry(user_id=user_id, total_connection_time = formatTime(my_totals.total_connection_time), total_mic_on_time = formatTime(my_totals.total_mic_on_time), rank = rank)
 
 def clearVcSessions(session: Session):
     session.query(VCSession).delete()
--- main.py
+++ main.py
@@ -12,7 +12,8 @@ from rich.logging import RichHandler
 from datetime import datetime
 from version import VERSION
 from database import init_db
-from database.crud import get_session, addUserCount, clearVcSessions, addVcSessions, endVcSessions, readVcSummary, updateServerNotificationChannel, readServerSetting, FutureDateError
+import database.crud as crud
+from database.crud import get_session
 
 load_dotenv()
 
@@ -107,7 +108,7 @@ async def on_ready():
 
     logger.debug("Debug ON")
     with get_session() as session:
-        clearVcSessions(session)
+        crud.clearVcSessions(session)
     await tree.sync()
 
 @client.event
@@ -152,65 +153,107 @@ async def on_message(message: discord.Message):
         await message.channel.send(response)
 
 
-@tree.command(
-    name = 'ping',
-    description = 'pingを返します'
-)
 async def ping(interaction: discord.Interaction):
     logger.debug(f"{interaction.user.id} executed /ping command in {f"guild id={interaction.guild.id}" if interaction.guild else f"{interaction.channel.type.name} id={interaction.channel.id}"}")
     with get_session() as session:
-        addUserCount(session, interaction.user.id)
+        crud.addUserCount(session, interaction.user.id)
     await interaction.response.send_message("pong!")
 
-@serverSettings.command(
-        name = 'notification-channel',
-        description = 'botの通知チャンネルを変更します。'
-)
-@app_commands.describe(
-    channel="通知するチャンネル"
-)
-async def notificationChannel(interaction: discord.Integration, channel: discord.TextChannel):
+@tree.command(name = 'ping', description = 'pingを返します')
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+async def ping_slash(interaction: discord.Interaction):
+    await ping(interaction = interaction)
+
+
+async def notification_channel(interaction: discord.Integration, channel: discord.TextChannel):
     logger.debug(f"{interaction.user.id} executed /notification-channel command in {f"guild id={interaction.guild.id}" if interaction.guild else f"{interaction.channel.type.name} id={interaction.channel.id}"}")
     with get_session() as session:
-        updateServerNotificationChannel(session, interaction.guild.id, channel.id)
+        crud.updateServerNotificationChannel(session, interaction.guild.id, channel.id)
     await interaction.response.send_message(f"通知チャンネルを <#{channel.id}> に設定しました！")
 
+@serverSettings.command(name = 'notification-channel', description = 'botの通知チャンネルを変更します。')
+@app_commands.describe(channel="通知するチャンネル")
+async def notification_channel_slash(interaction: discord.Integration, channel: discord.TextChannel):
+    await notification_channel(interaction = interaction, channel = channel)
 
-@tree.command(
-        name= 'vc-time',
-        description= 'あなたのvc接続時間を表示します。'
-)
-@app_commands.guild_only()
-@app_commands.describe(
-    channel="閲覧するチャンネル",
-    ephemeral="自分にしか表示しないかどうかです。defaultでTrueです。"
-)
-async def vcLog(interaction: discord.Interaction, channel: discord.VoiceChannel,year: app_commands.Range[int, 2025, 2099] = None, month: app_commands.Range[int, 1, 12] = None, ephemeral: bool=True):
+
+async def vc_log(interaction: discord.Interaction, channel: discord.VoiceChannel, year: app_commands.Range[int, 2025, 2099] = None, month: app_commands.Range[int, 1, 12] = None, ephemeral: bool = True):
     try:
         with get_session() as session:
-            connection_time, mic_on_time  = readVcSummary(session, interaction.guild.id, interaction.user.id, channel.id, year, month)
-    except FutureDateError:
+            connection_time, mic_on_time  = crud.readVcSummary(session, interaction.guild.id, interaction.user.id, channel.id, year, month)
+    except crud.FutureDateError:
         await interaction.response.send_message(f"ごめんね～\n私、未来のことはわかんないんだよね......\nその時まで一緒にいれると嬉しいな！", ephemeral=True)
+        return
+    except crud.NoDataError:
+        await interaction.response.send_message(f"{year or datetime.now().year}年 {month or datetime.now().month}月のデータがなかったよ！", ephemeral = ephemeral)
         return
     logger.debug(f"{interaction.user.id} queried vc-time for {channel.name}: Connection Time: {connection_time}, Mic Time: {mic_on_time}")
     if year is not None and month is None:
-        await interaction.response.send_message(f"{year or datetime.now().year}年に {channel.name} に接続していた時間の発表です！\n接続時間: {connection_time}\nミュート: {mic_on_time}", ephemeral=ephemeral)
+        await interaction.response.send_message(f"{year or datetime.now().year}年に <#{channel.id}> に接続していた時間の発表です！\n接続時間: {connection_time}\nミュート: {mic_on_time}", ephemeral = ephemeral)
     else:
-        await interaction.response.send_message(f"{year or datetime.now().year}年 {month or datetime.now().month}月に {channel.name} に接続していた時間の発表です！\n接続時間: {connection_time}\nミュート: {mic_on_time}", ephemeral=ephemeral)
+        await interaction.response.send_message(f"{year or datetime.now().year}年 {month or datetime.now().month}月に <#{channel.id}> に接続していた時間の発表です！\n接続時間: {connection_time}\nミュート: {mic_on_time}", ephemeral = ephemeral)
 
-@tree.command(
-    name = 'rps',
-    description = 'じゃんけんをします。'
-)
-async def rps(interaction: discord.Interaction, ):
+@tree.command(name= 'vc-time', description= 'あなたのvc接続時間を表示します。')
+@app_commands.guild_only()
+@app_commands.describe(channel="閲覧するチャンネル", ephemeral="自分にしか表示しないかどうかです。defaultでTrueです。")
+async def vc_log_slash(interaction: discord.Interaction, channel: discord.VoiceChannel, year: app_commands.Range[int, 2025, 2099] = None, month: app_commands.Range[int, 1, 12] = None, ephemeral: bool = True):
+    await vc_log(interaction = interaction, channel = channel, year = year, month = month, ephemeral = ephemeral)
+
+
+async def vc_rank(interaction: discord.Integration, channel: discord.VoiceChannel = None, year: app_commands.Range[int, 2025, 2099] = None, month: app_commands.Range[int, 1, 12] = None, ephemeral: bool = False):
+    channel_id = channel.id if channel is not None else None
+    user_nodata = False
+    try:
+        with get_session() as session:
+            vc_rank = crud.readVcRankEntries(session, interaction.guild.id, channel_id, year, month)
+            user_rank = crud.readUserVcRankEntry(session, interaction.guild.id, interaction.user.id, channel_id, year, month)
+    except crud.FutureDateError:
+        await interaction.response.send_message(f"ごめんね～\n私、未来のことはわかんないんだよね......\nその時まで一緒にいれると嬉しいな！", ephemeral=True)
+        return
+    except crud.NoDataError:
+        user_nodata = True
+    logger.debug(f"{interaction.user.id} queried vc-rank for {interaction.guild.id}/{channel_id}: {vc_rank}")
+    await interaction.response.defer(thinking=True)
+
+    lines = []
+    channel_display = f"<#{channel_id}>" if channel is not None else interaction.guild.name
+    lines.append(f"{year or datetime.now().year}年 {month or datetime.now().month}月に {channel_display} に接続していた人のランキングの発表です！")
+    if vc_rank.entries:
+        for entry in vc_rank.entries:
+            member = interaction.guild.get_member(entry.user_id)
+            if member is None:
+                member = await interaction.guild.fetch_member(entry.user_id)
+            name = member.display_name if member else "unknown"
+            lines.append(f"{entry.rank}位 {name} | 接続: {entry.total_connection_time} | マイク: {entry.total_mic_on_time}")
+    else:
+        lines.append(f"データなし")
+    if not user_nodata:
+        lines.append(f"{user_rank.rank}位 {name} | 接続: {user_rank.total_connection_time} | マイク: {user_rank.total_mic_on_time}")
+
+    await interaction.followup.send("\n".join(lines))
+
+@tree.command(name= 'vc-rank', description= 'あなたのvc接続時間を表示します。')
+@app_commands.guild_only()
+@app_commands.describe(channel="閲覧するチャンネル", ephemeral="自分にしか表示しないかどうかです。defaultでFalseです。")
+async def vc_rank_slash(interaction: discord.Integration, channel: discord.VoiceChannel = None, year: app_commands.Range[int, 2025, 2099] = None, month: app_commands.Range[int, 1, 12] = None, ephemeral: bool = False):
+        await vc_rank(interaction = interaction, channel = channel, year = year, month = month, ephemeral = ephemeral)
+
+
+async def rps(interaction: discord.Interaction):
     logger.debug(f"{interaction.user.id} executed /rps command in {f"guild id={interaction.guild.id}" if interaction.guild else f"{interaction.channel.type.name} id={interaction.channel.id}"}")
     with get_session() as session:
-        addUserCount(session, interaction.user.id)
+        crud.addUserCount(session, interaction.user.id)
     if random.randint(1, 100) == 1:
         await interaction.response.send_message(":hand_with_index_finger_and_thumb_crossed:")
     else:
         faces = ["✊", "✌️", "🖐️"]
         await interaction.response.send_message(f'{random.choice(faces)}')
+
+@tree.command(name = 'rps', description = 'じゃんけんをします。')
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+async def rps_slash(interaction: discord.Interaction, ):
+    await rps(interaction = interaction)
+
 
 def judge(player, bot):
     if player == bot:
@@ -241,43 +284,42 @@ class rpsMeView(discord.ui.View):
         await self.janken_result(interaction, "🖐️")
 
     async def janken_result(self, interaction: discord.Interaction, player_choice):
-        if random.randint(1, 160) == 1:
-            bot_choice = ":hand_with_index_finger_and_thumb_crossed:"
-        else:
-            faces = ["✊", "✌️", "🖐️"]
-            bot_choice = random.choice(faces)
-        result = judge(player_choice, bot_choice)
-        await interaction.message.edit(
-            content = f"わたし: {bot_choice} vs {player_choice} :あなた\n{result}",
-            view = None
-        )
+        try:
+            if random.randint(1, 160) == 1:
+                bot_choice = ":hand_with_index_finger_and_thumb_crossed:"
+            else:
+                faces = ["✊", "✌️", "🖐️"]
+                bot_choice = random.choice(faces)
+            result = judge(player_choice, bot_choice)
+            await interaction.message.edit(
+                content = f"わたし: {bot_choice} vs {player_choice} :あなた\n{result}",
+                view = None
+            )
+        except discord.Forbidden:
+            logger.debug(f"Missing Access when trying to edit message (ID: {interaction.message.id}) in channel (ID: {interaction.channel.id}) by user (ID: {interaction.user.id}")
+            pass
 
-@tree.command(
-    name = 'rps-me',
-    description = '私とじゃんけんをしよう！'
-)
-async def rpsMe(interaction: discord.Integration):
+
+async def rps_me(interaction: discord.Integration):
     logger.debug(f"{interaction.user.id} executed /rps-me command in {f"guild id={interaction.guild.id}" if interaction.guild else f"{interaction.channel.type.name} id={interaction.channel.id}"}")
     with get_session() as session:
-        addUserCount(session, interaction.user.id)
+        crud.addUserCount(session, interaction.user.id)
     if random.randint(1, 250) == 1:
         await interaction.response.send_message("zzz...")
     else:
         view = rpsMeView()
-        await interaction.response.send_message("じゃんけん...", view = view)
+        await interaction.response.send_message("じゃんけん...\n||現状権限が無いとエラーを吐くよ？ごめんね||", view = view)
 
-@tree.command(
-    name = 'dice',
-    description = 'サイコロを振ります'
-)
-@app_commands.describe(
-    roll="サイコロを振る回数です",
-    side="サイコロの面の数です"
-)
+@tree.command(name = 'rps-me', description = '私とじゃんけんをしよう！')
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=False)
+async def rps_me_slash(interaction: discord.Integration):
+    await rps_me(interaction = interaction)
+
+
 async def dice(interaction: discord.Interaction, roll: int, side: int):
     logger.debug(f"{interaction.user.id} executed /dice command in {f"guild id={interaction.guild.id}" if interaction.guild else f"{interaction.channel.type.name} id={interaction.channel.id}"}")
     with get_session() as session:
-        addUserCount(session, interaction.user.id)
+        crud.addUserCount(session, interaction.user.id)
     if side is None or roll is None:
         logger.error(f'Not a valid parameter: roll: {roll} side: {side}')
         await interaction.response.send_message("必要なオプションがが指定されていません。",ephemeral=True)
@@ -298,39 +340,53 @@ async def dice(interaction: discord.Interaction, roll: int, side: int):
             logger.exception(f'Error in random calculation: roll: {roll} side: {side}')
             await interaction.followup.send("わかんないよぅ；；\nbot管理者まで連絡ください。")
 
-@tree.command(
-    name = 'chinchiro',
-    description = 'チンチロを振ります'
-)
+@tree.command(name = 'dice', description = 'サイコロを振ります')
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+@app_commands.describe(roll="サイコロを振る回数です", side="サイコロの面の数です")
+async def dice_slash(interaction: discord.Interaction, roll: int, side: int):
+    await dice(interaction = interaction, roll = roll, side = side)
+
+
 async def chinchiro(interaction: discord.Interaction):
     logger.debug(f"{interaction.user.id} executed /chinchiro command in {f"guild id={interaction.guild.id}" if interaction.guild else f"{interaction.channel.type.name} id={interaction.channel.id}"}")
     with get_session() as session:
-        addUserCount(session, interaction.user.id)
+        crud.addUserCount(session, interaction.user.id)
     if random.randint(1, 50) == 1:
         await interaction.response.send_message("台からサイコロが落ちた！")
     else:
         await interaction.response.send_message(f'{random.randint(1, 6)}  {random.randint(1, 6)}  {random.randint(1, 6)}')
 
-@tree.command(
-    name = 'dice-poker',
-    description = '一般的なダイスポーカーを振ります'
-)
-async def dicePoker(interaction: discord.Interaction):
+@tree.command(name = 'chinchiro', description = 'チンチロを振ります')
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+async def chinchiro_slash(interaction: discord.Interaction):
+    await chinchiro(interaction = interaction)
+
+
+async def dice_poker(interaction: discord.Interaction):
     logger.debug(f"{interaction.user.id} executed /dice-poker command in {f"guild id={interaction.guild.id}" if interaction.guild else f"{interaction.channel.type.name} id={interaction.channel.id}"}")
     with get_session() as session:
-        addUserCount(session, interaction.user.id)
+        crud.addUserCount(session, interaction.user.id)
     faces = ["9", "10", "J", "Q", "K", "A"]
     await interaction.response.send_message(f'{random.choice(faces)}  {random.choice(faces)}  {random.choice(faces)}  {random.choice(faces)}  {random.choice(faces)}')
 
-@tree.command(
-    name = 'dice-poker-stgr',
-    description = 'ストグラのカジノで行われているダイスポーカーを振ります'
-)
-async def dicePokerStgr(interaction: discord.Integration):
+@tree.command(name = 'dice-poker', description = '一般的なダイスポーカーを振ります')
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+@app_commands.allowed_contexts(guilds=True, dms=True, private_channels=True)
+async def dice_poker_slash(interaction: discord.Interaction):
+    await dice_poker(interaction = interaction)
+
+
+async def dice_poker_stgr(interaction: discord.Integration):
     logger.debug(f"{interaction.user.id} executed /dice-poker-stgr command in {f"guild id={interaction.guild.id}" if interaction.guild else f"{interaction.channel.type.name} id={interaction.channel.id}"}")
     with get_session() as session:
-        addUserCount(session, interaction.user.id)
+        crud.addUserCount(session, interaction.user.id)
     await interaction.response.send_message(f'{random.randint(1, 6)}  {random.randint(1, 6)}  {random.randint(1, 6)}  {random.randint(1, 6)}  {random.randint(1, 6)}')
+
+@tree.command(name = 'dice-poker-stgr', description = 'ストグラのカジノで行われているダイスポーカーを振ります')
+@app_commands.user_install()
+async def dice_poker_stgr_slash(interaction: discord.Integration):
+    await dice_poker_stgr(interaction = interaction)
+
 
 @client.event
 async def on_voice_state_update(member, before, after):
@@ -338,35 +394,38 @@ async def on_voice_state_update(member, before, after):
     
     logger.debug(f"Event triggered: {member.display_name}, Before: {before.channel}, After: {after.channel}")
     with get_session() as session:
-        alert_channel_id = readServerSetting(session, member.guild.id).notification_channel
-    alert_channel = client.get_channel(alert_channel_id)
+        alert_channel_id = crud.readServerSetting(session, member.guild.id).notification_channel
+    alert_channel = client.get_channel(alert_channel_id) or member.guild.system_channel
     if alert_channel is None:
         logger.error(f"Alert channel with ID {alert_channel_id} not found or no access.")
+        return
+    
+    if not alert_channel.permissions_for(member.guild.me).send_messages:
         return
 
     if before.channel is None and after.channel is not None:
         msg = f'{member.display_name} が {after.channel.name} に参加しました。'
         with get_session() as session:
-            addVcSessions(session, member.guild.id, member.id, after.channel.id, after.self_mute)
+            crud.addVcSessions(session, member.guild.id, member.id, after.channel.id, after.self_mute)
     elif after.channel is None and before.channel is not None:
         msg = f'{member.display_name} が {before.channel.name} から退出しました。'
         with get_session() as session:
-            endVcSessions(session, member.guild.id, member.id, before.channel.id, before.self_mute, startup_time)
+            crud.endVcSessions(session, member.guild.id, member.id, before.channel.id, before.self_mute, startup_time)
     elif before.channel is not None and after.channel is not None:
         if before.channel.id != after.channel.id:
             msg = f'{member.display_name} が {before.channel.name} から {after.channel.name} に移動しました。'
             with get_session() as session:
-                endVcSessions(session, member.guild.id, member.id, before.channel.id, before.self_mute, startup_time)
-                addVcSessions(session, member.guild.id, member.id, after.channel.id, after.self_mute)
+                crud.endVcSessions(session, member.guild.id, member.id, before.channel.id, before.self_mute, startup_time)
+                crud.addVcSessions(session, member.guild.id, member.id, after.channel.id, after.self_mute)
         elif before.self_mute != after.self_mute:
             if before.self_mute:
                 with get_session() as session:
-                    endVcSessions(session, member.guild.id, member.id, before.channel.id, before.self_mute, startup_time)
-                    addVcSessions(session, member.guild.id, member.id, after.channel.id, after.self_mute)
+                    crud.endVcSessions(session, member.guild.id, member.id, before.channel.id, before.self_mute, startup_time)
+                    crud.addVcSessions(session, member.guild.id, member.id, after.channel.id, after.self_mute)
             elif after.self_mute:
                 with get_session() as session:
-                    endVcSessions(session, member.guild.id, member.id, before.channel.id, before.self_mute, startup_time)
-                    addVcSessions(session, member.guild.id, member.id, after.channel.id, after.self_mute)
+                    crud.endVcSessions(session, member.guild.id, member.id, before.channel.id, before.self_mute, startup_time)
+                    crud.addVcSessions(session, member.guild.id, member.id, after.channel.id, after.self_mute)
 
     if msg is not None:
         logger.debug(f'Send message: {msg}')
